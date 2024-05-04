1d6a3371eb127b2ef59807bbb6fe36c4b4aab1be
RabbanMehamood <shaik.mehamood786@gmail.com>
2024-05-04 18:51:00 +0000

    initial commit

---
+++ src/components/ThumbnailItem/index.js
@@ -0,0 +1,25 @@
+import './index.css'
+
+const ThumbnailItem = props => {
+    const {thumbnail,onClickThumbnail} = props
+    const {id,thumbnailUrl} = thumbnail 
+
+    const onClickThumbnailItem = () =>{
+        onClickThumbnail(id)
+    }
+    return (
+        <li className = "thumbnail-container">
+        <button 
+        className="thumbnail-button"
+        type = "button"
+        onClick={onClickThumbnailItem}
+        >
+         <img className="thumbnail-image" src={thumbnailUrl} alt="thumbnail" />
+        </button>
+        
+        </li>
+    )
+
+}
+
+export default ThumbnailItem
